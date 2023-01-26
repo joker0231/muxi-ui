f55e743ca9288a5cc8e9be3bde9147b74f73bd03
suyapeng <suyapeng@wps.cn>
2023-01-26 10:31:31 +0800

    chore(pub): 修复eslint配置问题，修正文件规范

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,104 +1,118 @@
-import React from "react"
-import {PureComponent} from "react";
-import {IBaseInputProps} from "./interface";
-import classNames from "classnames";
+import React, { PureComponent } from 'react'
 
-class Input<IProps> extends PureComponent<IBaseInputProps & IProps>{    // todo：感觉这个ts有问题，应该是覆盖
-  protected inputRef = React.createRef<HTMLInputElement>()
+import classNames from 'classnames'
+import { type IBaseInputProps } from './interface'
 
-  public getInput(): HTMLInputElement {
-    return this.inputRef.current as HTMLInputElement  // 去除null，本组件inputRef一定存在
-  }
+class Input<IProps> extends PureComponent<IBaseInputProps & IProps> {
+    // todo：感觉这个ts有问题，应该是覆盖
+    protected inputRef = React.createRef<HTMLInputElement>()
 
-  // 处理onChange事件，转换到外部的onChange
-  protected handleInputChange = () => {
-    if(this.props.onChange) {
-      this.props.onChange(this.getInput().value, this.isChineseInputting)
+    public getInput(): HTMLInputElement {
+        return this.inputRef.current as HTMLInputElement // 去除null，本组件inputRef一定存在
     }
-  }
 
-  /**
-   * 处理焦点
-   * @param isSelectAll 是否全选
-   * @param isEnd 是否把光标定到最后
-   */
-  public focus(isSelectAll=false, isEnd = false):void {
-    const input = this.getInput()
-    input.focus()
-    const {value} = input
-    if(isSelectAll && value.length > 0) {  // 全选和光标到最后只能二选一
-      input.select()
-    } else if (isEnd) {
-      input.value = value + ''
-      input.value = value
+    // 处理onChange事件，转换到外部的onChange
+    protected handleInputChange = () => {
+        if (this.props.onChange != null) {
+            this.props.onChange(this.getInput().value, this.isChineseInputting)
+        }
     }
-  }
 
-  // 处理中文预输入
-  public isChineseInputting = false  // 提供外部查询是否正在中文预输入
-  protected handleCompositionStart = () => {  // 中文预输入开始
-    this.isChineseInputting =  true
-  }
-  protected handleCompositionEnd = () => {  // 中文预输入结束
-    this.isChineseInputting = false
-    this.handleInputChange()
-  }
+    /**
+     * 处理焦点
+     * @param isSelectAll 是否全选
+     * @param isEnd 是否把光标定到最后
+     */
+    public focus(isSelectAll = false, isEnd = false): void {
+        const input = this.getInput()
+        input.focus()
+        const { value } = input
+        if (isSelectAll && value.length > 0) {
+            // 全选和光标到最后只能二选一
+            input.select()
+        } else if (isEnd) {
+            input.value = `${value}`
+            input.value = value
+        }
+    }
 
-  // 处理键盘按键
-  protected handleKeyDown = (e: React.KeyboardEvent) => {
-    const {onEnter, onEsc, onKeyDown} = this.props
-    if (onKeyDown) {
-      onKeyDown(e)
-    }
-    const key = e.key  // todo 封装KeyHelper，将一些code转换为Enter等寓意变量
-    if(onEnter && key === 'Enter') {
-      onEnter()
-      return
-    }
-    if(onEsc && key === 'Esc') {
-      onEsc()
-      return
-    }
-  }
+    // 处理中文预输入
+    public isChineseInputting = false // 提供外部查询是否正在中文预输入
 
-  public render() {
-    const {className, iconType, disabled, errorText, style, ...inputProps} = this.props
-    if(inputProps.hasOwnProperty('value')  && (inputProps.value === null || inputProps.value === undefined)) { // 如果inputProps没有value字段，加一个空的
-      // @ts-ignore
-      inputProps.value = ''
+    protected handleCompositionStart = () => {
+        // 中文预输入开始
+        this.isChineseInputting = true
     }
-    // 删除多余事件（不直接透传到input上）
-    delete inputProps.autoFocus
-    delete inputProps.autoSelect
-    delete inputProps.onEnter
-    delete inputProps.onEsc
-    const cs = classNames({
-      'wowui-input': 1,
-      'input-has-error': errorText,
-      'input-has-icon': iconType,
-      'disabled': disabled,
-    }, className)
-    console.log(disabled, 'syp-disabled')
-    return (
-        <div className={cs} style={style}>
-          {
-            !!iconType &&
-              <div className="input-icon-warp">
-                {/*todo: Icon组件*/}
-                  {/*<Icon iconType={iconType}/>*/}
-              </div>
-          }
-          <input ref={this.inputRef} type="text" {...inputProps}
-                 onChange={this.handleInputChange} onKeyDown={this.handleKeyDown}
-                 onCompositionStart={this.handleCompositionStart}
-                 onCompositionEnd={this.handleCompositionEnd} disabled={disabled}/>
-          {
-            !!errorText &&
-              <div className="input-error-text">{errorText}</div>
-          }
-        </div>
-    )
-  }
+
+    protected handleCompositionEnd = () => {
+        // 中文预输入结束
+        this.isChineseInputting = false
+        this.handleInputChange()
+    }
+
+    // 处理键盘按键
+    protected handleKeyDown = (e: React.KeyboardEvent) => {
+        const { onEnter, onEsc, onKeyDown } = this.props
+        if (onKeyDown) {
+            onKeyDown(e)
+        }
+        const { key } = e // todo 封装KeyHelper，将一些code转换为Enter等寓意变量
+        if (onEnter != null && key === 'Enter') {
+            onEnter()
+            return
+        }
+        if (onEsc != null && key === 'Esc') {
+            onEsc()
+        }
+    }
+
+    public render() {
+        const { className, iconType, disabled, errorText, style, ...inputProps } = this.props
+        if (
+            Object.prototype.hasOwnProperty.call(inputProps, 'value') &&
+            (inputProps.value === null || inputProps.value === undefined)
+        ) {
+            // 如果inputProps没有value字段，加一个空的
+            // @ts-expect-error 测试1
+            inputProps.value = ''
+        }
+        // 删除多余事件（不直接透传到input上）
+        delete inputProps.autoFocus
+        delete inputProps.autoSelect
+        delete inputProps.onEnter
+        delete inputProps.onEsc
+        const cs = classNames(
+            {
+                'wowui-input': 1,
+                'input-has-error': errorText,
+                'input-has-icon': iconType,
+                disabled
+            },
+            className
+        )
+        console.log(disabled, 'syp-disabled')
+        return (
+            <div className={cs} style={style}>
+                {!(iconType == null) && (
+                    <div className="input-icon-warp">
+                        {/* todo: Icon组件 */}
+                        {/* <Icon iconType={iconType}/> */}
+                    </div>
+                )}
+                <input
+                    ref={this.inputRef}
+                    type="text"
+                    {...inputProps}
+                    onChange={this.handleInputChange}
+                    onKeyDown={this.handleKeyDown}
+                    onCompositionStart={this.handleCompositionStart}
+                    onCompositionEnd={this.handleCompositionEnd}
+                    disabled={disabled}
+                />
+                {!(errorText == null) && <div className="input-error-text">{errorText}</div>}
+            </div>
+        )
+    }
 }
 
 export default Input
--- src/components/Input/Pc/FieldInput/FieldInput.tsx
+++ src/components/Input/Pc/FieldInput/FieldInput.tsx
@@ -1,54 +1,64 @@
 /**
  * 带字段的输入框
  */
-import Input from "../../Input";
-import {IFieldInputProps} from "../../interface";
-import classNames from "classnames";
-import React from "react";
+import classNames from 'classnames'
+import React from 'react'
+import Input from '../../Input'
+import { type IFieldInputProps } from '../../interface'
 
 class FieldInput extends Input<IFieldInputProps> {
-  // 这里所有其他方法完全一直，只是添加了this.props.filedName，只用重写super.render方法即可
-  public render(): JSX.Element {
-    console.log(this.props.filedName)
-    // return super.render();
-    const {className, iconType, disabled, errorText, style, filedName,...inputProps} = this.props
-    if(inputProps.hasOwnProperty('value')  && (inputProps.value === null || inputProps.value === undefined)) { // 如果inputProps没有value字段，加一个空的
-      // @ts-ignore
-      inputProps.value = ''
-    }
-    // 删除多余事件（不直接透传到input上）
-    delete inputProps.autoFocus
-    delete inputProps.autoSelect
-    delete inputProps.onEnter
-    delete inputProps.onEsc
-    const cs = classNames({
-      'wowui-input': 1,
-      'input-has-error': errorText,
-      'input-has-icon': iconType,
-      'disabled': disabled,
-    }, className)
-    console.log(disabled, 'syp-disabled')
-    return (
-      <div className={cs} style={style}>
-        {
-          !!iconType &&
-            <div className="input-icon-warp">
-              {/*todo: Icon组件*/}
-              {/*<Icon iconType={iconType}/>*/}
+    // 这里所有其他方法完全一直，只是添加了this.props.filedName，只用重写super.render方法即可
+    public render(): JSX.Element {
+        console.log(this.props.filedName)
+        // return super.render();
+        const { className, iconType, disabled, errorText, style, filedName, ...inputProps } =
+            this.props
+        if (
+            Object.prototype.hasOwnProperty.call(inputProps, 'value') &&
+            (inputProps.value === null || inputProps.value === undefined)
+        ) {
+            // 如果inputProps没有value字段，加一个空的
+            // @ts-expect-error: 测试
+            inputProps.value = ''
+        }
+        // 删除多余事件（不直接透传到input上）
+        delete inputProps.autoFocus
+        delete inputProps.autoSelect
+        delete inputProps.onEnter
+        delete inputProps.onEsc
+        const cs = classNames(
+            {
+                'wowui-input': 1,
+                'input-has-error': errorText,
+                'input-has-icon': iconType,
+                disabled
+            },
+            className
+        )
+        console.log(disabled, 'syp-disabled')
+        return (
+            <div className={cs} style={style}>
+                {!(iconType == null) && (
+                    <div className="input-icon-warp">
+                        {/* todo: Icon组件 */}
+                        {/* <Icon iconType={iconType}/> */}
+                    </div>
+                )}
+                <div>{filedName}：</div>
+                <input
+                    ref={this.inputRef}
+                    type="text"
+                    {...inputProps}
+                    onChange={this.handleInputChange}
+                    onKeyDown={this.handleKeyDown}
+                    onCompositionStart={this.handleCompositionStart}
+                    onCompositionEnd={this.handleCompositionEnd}
+                    disabled={disabled}
+                />
+                {!(errorText == null) && <div className="input-error-text">{errorText}</div>}
             </div>
-        }
-        <div>{filedName}：</div>
-        <input ref={this.inputRef} type="text" {...inputProps}
-               onChange={this.handleInputChange} onKeyDown={this.handleKeyDown}
-               onCompositionStart={this.handleCompositionStart}
-               onCompositionEnd={this.handleCompositionEnd} disabled={disabled}/>
-        {
-          !!errorText &&
-            <div className="input-error-text">{errorText}</div>
-        }
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default FieldInput
